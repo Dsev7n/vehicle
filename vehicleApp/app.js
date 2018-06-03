4828d111db0a86addfec53ef5fb10ab3e5b74591
xiaomian <937548468@qq.com>
2018-06-03 10:38:45 +0800

    添加配置

--- vehicleApp/app.js
+++ vehicleApp/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 
+app.use(express.static('../vehicle/dist'))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
